bb6235dc33aac3d9973b83a49660e4ae8c539744
Gustavo Gonzalez <gustavogonzalez22.011011@gmail.com>
2025-11-20 10:44:54 -0300

    Weather app: update - finished day 3 goal

--- content/week 2/weather-app/script.js
+++ content/week 2/weather-app/script.js
@@ -18,6 +18,36 @@ getWeatherBtn.addEventListener('click', () => {
     getWeather(city);
 });
 
+function padStart(value) {
+    return String(value).padStart(2, '0');
+}
+
+function formatTime(time) {
+    const date = new Date(time);
+
+    const hours24 = date.getHours();
+    let hours12;
+    let period;
+
+    period = hours24 < 12 ? 'AM' : 'PM';
+
+    if (hours24 === 0) {
+        hours12 = 12;
+    } else if (hours24 <= 12) {
+        hours12 = hours24;
+    } else {
+        hours12 = hours24 - 12;
+    }
+
+    const minutes = padStart(date.getMinutes());
+    const day = padStart(date.getDate());
+    const month = padStart(date.getMonth() + 1);
+    const year = date.getFullYear();
+    const hours = padStart(hours12);
+
+    return `${hours}:${minutes} ${period} - ${day}/${month}/${year}`;
+}
+
 async function getWeather(city) {
     statusText.textContent = 'Loading...';
     weatherBox.classList.add('hidden');
@@ -39,12 +69,13 @@ async function getWeather(city) {
         const { temperature, windspeed, time } = weatherData.current_weather;
 
         cityName.textContent = name;
-        temp.textContent = temperature;
-        wind.textContent = windspeed;
-        timeEl.textContent = time;
+        temp.textContent = `${temperature}`;
+        wind.textContent = `${windspeed}`;
+        timeEl.textContent = formatTime(time);
 
         weatherBox.classList.remove('hidden');
         statusText.textContent = '';
+        cityInput.value = '';
     } catch (error) {
         console.error(error);
         statusText.textContent = 'Error fetching weather data.';
