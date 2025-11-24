2b24edee5c437856686da3e7b534ef45c56757f1
Dev-Gus <gustavogonzalez22.011011@gmail.com>
2025-11-24 07:32:29 -0300

    Weather app changes applied. clear input & search city pressing enter key

--- content/week 2/weather-app/js/controller.js
+++ content/week 2/weather-app/js/controller.js
@@ -1,58 +1,77 @@
 import ui from "./ui.js";
 import { getCoordinates, getWeatherData } from "./api.js";
+import { cityInput } from "./ui.js";
 
-const getWeatherBtn = document.getElementById('getWeatherBtn');
+const getWeatherBtn = document.getElementById("getWeatherBtn");
 
 export const fetchWeather = async (city) => {
-    try {
-        if (!city) {
-            throw new Error('Input must be a city name');
-        }
-
-        const { latitude, longitude, name } = await getCoordinates(city);
-
-        const weather = await getWeatherData(latitude, longitude);
-
-        return { name, latitude, longitude, weather }
-    } catch (error) {
-        throw error;
+  try {
+    if (!city) {
+      throw new Error("Input must be a city name");
     }
-}
+
+    const { latitude, longitude, name } = await getCoordinates(city);
+
+    const weather = await getWeatherData(latitude, longitude);
+
+    return { name, latitude, longitude, weather };
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const renderWeather = (data) => {
-    if (!data || !data.weather) {
-        ui.showError('Missing weather data');
-        return;
-    }
+  if (!data || !data.weather) {
+    ui.showError("Missing weather data");
+    return;
+  }
 
-    ui.updateWeatherUI(data.name, data.weather);
-    ui.showSuccess();
-}
+  ui.updateWeatherUI(data.name, data.weather);
+  ui.showSuccess();
+};
 
 export const initApp = async () => {
-    getWeatherBtn.addEventListener('click', async () => {
-        ui.showLoading();
-        try {
-            const city = ui.getCityInput();
+  getWeatherBtn.addEventListener("click", () => {
+    handleWeatherRequest();
+  });
 
-            const cityWeather = await fetchWeather(city);
-            renderWeather(cityWeather);
-            localStorage.setItem('lastCity', cityWeather.name);
-        } catch (error) {
-            console.error(error);
-            ui.showError(error.message);
-        }
-    });
+  cityInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
 
-    try {
-        const lastCityStored = localStorage.getItem('lastCity');
-        if (lastCityStored) {
-            ui.showLoading();
-            const cityWeather = await fetchWeather(lastCityStored);
-            renderWeather(cityWeather);
-        }
-    } catch (error) {
-        console.error(error);
-        ui.showError(error.message);
+      handleWeatherRequest();
     }
+  });
+
+  try {
+    const lastCityStored = localStorage.getItem("lastCity");
+    if (lastCityStored) {
+      ui.showLoading();
+      const cityWeather = await fetchWeather(lastCityStored);
+      renderWeather(cityWeather);
+    }
+  } catch (error) {
+    console.error(error);
+    ui.showError(error.message);
+  }
+};
+
+const handleWeatherRequest = async () => {
+  const city = ui.getCityInput();
+  if(!city) {
+    ui.showError('Please entera city');
+    return;
+  }
+
+  ui.showLoading();
+  try {
+    const cityWeather = await fetchWeather(city);
+    renderWeather(cityWeather);
+    localStorage.setItem("lastCity", cityWeather.name);
+
+    ui.clearInput();
+  } catch (error) {
+    console.error(error);
+    ui.showError(error.message);
+  }
 };
--- content/week 2/weather-app/js/ui.js
+++ content/week 2/weather-app/js/ui.js
@@ -1,42 +1,47 @@
-const cityInput = document.getElementById('cityInput');
-const statusText = document.getElementById('status');
-const weatherBox = document.getElementById('weatherResult');
+export const cityInput = document.getElementById("cityInput");
+const statusText = document.getElementById("status");
+const weatherBox = document.getElementById("weatherResult");
 
-const cityNameEl = document.getElementById('cityName');
-const tempEl = document.getElementById('temp');
-const windEl = document.getElementById('wind');
-const timeEl = document.getElementById('time');
+const cityNameEl = document.getElementById("cityName");
+const tempEl = document.getElementById("temp");
+const windEl = document.getElementById("wind");
+const timeEl = document.getElementById("time");
 
 const ui = {
-    getCityInput: () => cityInput?.value.trim(),
-    showLoading: () => {
-        statusText?.className = '';
-        statusText?.innerHTML = `<div class='loader'></div>`;
-        weatherBox?.classList.add('hidden');
-    },
-    showError: (msg) => {
-        statusText?.className = 'error';
-        statusText?.textContent = msg;
-        weatherBox?.classList.add('hidden');
-    },
-    showSuccess: () => {
-        statusText?.className = 'success';
-        statusText?.textContent = '';
-    },
-    updateWeatherUI: (name, weather) => {
-        const { temperature, windspeed, timeFormatted } = weather;
+  getCityInput: () => cityInput?.value.trim(),
 
-        cityNameEl?.textContent = name;
-        tempEl?.textContent = temperature;
-        windEl?.textContent = windspeed;
-        timeEl?.textContent = timeFormatted;
+  showLoading: () => {
+    if (statusText) statusText.className = "";
+    if (statusText) statusText.innerHTML = `<div class='loader'></div>`;
+    if (weatherBox) weatherBox.classList.add("hidden");
+  },
 
-        weatherBox?.classList.remove('hidden');
-        ui.showSuccess();
-    }, 
-    clearInput: () => {
-        cityInput?.value = '';
-    }
+  showError: (msg) => {
+    if (statusText) statusText.className = "error";
+    if (statusText) statusText.textContent = msg;
+    if (weatherBox) weatherBox.classList.add("hidden");
+  },
+
+  showSuccess: () => {
+    if (statusText) statusText.className = "success";
+    if (statusText) statusText.textContent = "";
+  },
+
+  updateWeatherUI: (name, weather) => {
+    const { temperature, windspeed, timeFormatted } = weather;
+
+    if (cityNameEl) cityNameEl.textContent = name;
+    if (tempEl) tempEl.textContent = temperature;
+    if (windEl) windEl.textContent = windspeed;
+    if (timeEl) timeEl.textContent = timeFormatted;
+
+    if (weatherBox) weatherBox.classList.remove("hidden");
+    ui.showSuccess();
+  },
+
+  clearInput: () => {
+    if (cityInput) cityInput.value = "";
+  },
 };
 
 export default ui;
